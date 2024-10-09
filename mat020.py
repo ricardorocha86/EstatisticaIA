98e92a887182cbd5e10884341110dad2fd2e0369
Ricardo Rocha <ricardo8610@gmail.com>
2024-10-08 21:44:31 -0300

    Add files via upload

---
+++ mat020.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+
+st.set_page_config(
+    page_title="MAT020",
+    page_icon="🐵",
+    layout="centered",
+    initial_sidebar_state="expanded")
+ 
+
+st.title('MAT020')
+ 
