30c63a8114d5531c142ec9157a584dde7cdc5d14
Ricardo Rocha <ricardo8610@gmail.com>
2024-10-15 22:55:18 -0300

    Add files via upload

--- mat020.py
+++ mat020.py
@@ -4,11 +4,11 @@ import pandas as pd
 import re
 from openai import OpenAI
 
-st.set_page_config(
-    page_title="Material de Aula MAT020",
-    page_icon="📚",
-    layout="wide",
-    initial_sidebar_state="expanded")
+st.set_page_config(page_title = "Estatística IA",
+                   page_icon = "🐵",
+                   layout = "wide",
+                   initial_sidebar_state = "expanded")
+    
 
  
 def st_markdown(markdown_string):
@@ -37,59 +37,101 @@ diretorio_aulas = 'aulas'
 # Busca as aulas no diretório
 aulas = buscar_aulas(diretorio_aulas)
 
+def aux(x):
+    return x[:-4]
 # Widget de seleção de aula
-aula_selecionada = st.sidebar.selectbox('Selecione uma aula:', aulas)
+aula_selecionada = st.sidebar.selectbox('Selecione uma aula:', aulas, format_func = aux)
  
 caminho_aula = os.path.join(diretorio_aulas, aula_selecionada)
 conteudo_aula = ler_aula(caminho_aula)
 st_markdown(conteudo_aula)
  
-
-abas = ['Resumo AI', 'Lista de Insights AI', 'Exercícios AI']
+st.divider()
+abas = ['Exercícios AI', 'Lista de Insights AI', 'Quiz AI']
 
 aba1, aba2, aba3 = st.tabs(abas)
 
 with aba1:
+
     st.header(abas[0]) 
+    if st.button('Gerar Lista de Exercícios dessa Lição', type = 'primary'): 
+
+        # Configurações de API 
+        openai_api_key = st.secrets["OPENAI_API_KEY"]
+        client = OpenAI(api_key=openai_api_key)
+        modelo = 'gpt-4o-mini'
+         
+        # Mensagem inicial do assistente no chat
+        prompt = f"""
+        Você é um assistente de professor cuja tarefa é escrever uma lista de exercicios baseado no <material de aula> abaixo.
+        Você deve fazer 3 exercícios abertos e 3 exercícios de teste. Nos abertos, deve ser feita uma pergunta aberta. 
+        Nos exercicios do tipo teste, deve ter uma pergunta e 4 alternativas de resposta, da qual apenas uma seja correta.
+        Cada alternativa deve estar em uma linha. 
+        Não diga qual resposta é a correta. 
+        O output deve conter os 3 exercicios abertos seguidos dos 3 exercicios de teste, separados por sessão. 
+        No seu output, deve contar apenas o exercício, nada a mais, nada a menos. 
+        Use formatação quando necessário, use negrito e italico para destacar o que for importante e emojis quando pertinente. 
+
+        <material de aula>
+        {conteudo_aula}
+        </material de aula>
+         """ 
+
+        # Faz uma requisição à API OpenAI para gerar a resposta do assistente
+        with st.chat_message("assistant", avatar = '🐵'):
+            stream = client.chat.completions.create(
+                model=modelo,
+                messages= [{"role": "user", "content": prompt}],
+                stream=True
+            )
+
+            # Exibe a resposta em tempo real
+            response = st.write_stream(stream)
+
+
 with aba2:
     st.header(abas[1]) 
+
+    if st.button('Gerar Lista de Insights dessa Lição', type = 'primary'): 
+
+        # Configurações de API 
+        openai_api_key = st.secrets["OPENAI_API_KEY"]
+        client = OpenAI(api_key=openai_api_key)
+        modelo = 'gpt-4o-mini'
+         
+        # Mensagem inicial do assistente no chat
+        prompt = f"""Você é um assistente que tem a função de utilizar um <material de aula> e processar seguindo as seguintes instruções:
+        - Leia e compreenda integralmente o material de aula para captar o contexto, os tópicos abordados e a progressão dos conceitos.
+        - Escreva um output que seja uma coleção de 10 insights sobre a aula.
+        - Use bullet points para cada insight. 
+        - use texto normal nos titulos das sessões. 
+        - cada insight deve ter no máximo 140 caracteres.
+        - seu output deve ser apenas, e somente apenas, a lista de insights.
+        - use negrito e italico para destacar o que for importante.
+
+        O material da aula é o seguinte:
+
+        <material de aula>
+        {conteudo_aula}
+        </material de aula>
+        """
+
+        # Faz uma requisição à API OpenAI para gerar a resposta do assistente
+        with st.chat_message("assistant", avatar = '🐵'):
+            stream = client.chat.completions.create(
+                model=modelo,
+                messages= [{"role": "user", "content": prompt}],
+                stream=True
+            )
+
+            # Exibe a resposta em tempo real
+            response = st.write_stream(stream)
+
+
+
 with aba3:
     st.header(abas[2])  
+    st.write('Em breve 🚧')
 
+    
 
-# Configurações de API 
-openai_api_key = st.secrets["OPENAI_API_KEY"]
-client = OpenAI(api_key=openai_api_key)
-
- 
-# Configurações de modelo e carregamento de instruções do assistente
-modelo = 'gpt-4o-mini'
- 
-
-# Mensagem inicial do assistente no chat
-prompt = f"""
-Você é um assistente de professor cuja tarefa é escrever exercicios baseado no <material de aula>.
-O exercício deve aleatorio, mas dentro dos topicos presentes no material de aula. 
-O exercicio deve ser um teste, com 4 alternativas de resposta, da qual apenas uma seja correta. 
-O teste deve ter um nivel de dificuldade médio para desafiador.
-No seu output, deve contar apenas o exercício, nada a mais, nada a menos. 
-Use formatação para destacar o que for importante e emojis quando pertinente (nao exagere).
-Não apresente a resposta correta. 
-Invente uma historia de background para o exercicio, dando algum contexto interessante, mas seja breve!. 
-
-<material de aula>
-{conteudo_aula}
-</material de aula>
- """ 
-
-if st.button('Gerar Exercício AI'): 
-    # Faz uma requisição à API OpenAI para gerar a resposta do assistente
-    with st.chat_message("assistant"):
-        stream = client.chat.completions.create(
-            model=modelo,
-            messages= [{"role": "user", "content": prompt}],
-            stream=True
-        )
-
-        # Exibe a resposta em tempo real
-        response = st.write_stream(stream)
